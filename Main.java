e0869a950d880b1923edb8137dab05e67d5db233
Mansur <165130890+Mansurrv@users.noreply.github.com>
2024-12-26 10:54:14 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,17 @@ public class Main {
 
     public static void Main(String[] args){
 
-        Participant part1 = new Participant("Dosymzhan", "Sheri");
-        Participant part2 = new Participant("Turganber", "Nurkhan");
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
+        for(int i=0; i<=count; i++){
+            System.out.println("Enter your name,surname: ");
+            Scanner scan2 = new Scanner(System.in);
+            String name = scan2.nextLine();
+            System.out.println("Enter your surname: ");
+            Scanner scan3 = new Scanner(System.in);
+            String surname = scan3.nextLine();
+            Participant part1 = new Participant(name, surname);
+        }
 
         Event date1 = new Event(12,45,9);
 
